6fc7e017e4436cc0ecb3f366b87cefc3c184d20d
Joyce Pang <joycepang@yahoo.com>
2021-06-22 20:25:00 +0800

    modified version

--- password.py
+++ password.py
@@ -8,12 +8,17 @@
 password = 'a123456'
 i = 3 # 剩余机会
 while i > 0:
+	i = i - 1
 	pwd = input('Please input your password: ')
 	if pwd == password:
 		print('Login successfully!')
 		break # 逃出回圈
 	else:
-		i = i - 1
-		print('Password is wrong! You have ', i, 'chances')
+		print('Password is wrong!')
+		if i > 0:
+			print('You have ', i, 'chances')
+		else:
+			print('No more chance! Your account is locked!')
+
 
 
